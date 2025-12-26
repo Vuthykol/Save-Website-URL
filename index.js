faa13b223116bbb7a43f8d2d91245e1fc75e3c0d
Song Vuthykol <vuthykolsong0@gmail.com>
2025-12-26 18:31:52 +0700

    Implement local storage for leads and delete option
    Added functionality to store leads in local storage and clear them.

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-let inputBtn = document.getElementById("input-btn") 
-
-const myLead = []
+let inputBtn = document.getElementById("input-btn")
+let deleteBtn = document.getElementById("delete-btn")
 const inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
-inputBtn.addEventListener("click", function(){
-    myLead.push(inputEl.value)
-     renderLeads()
-     clearInput()
-    })
-    function renderLeads(){
-   let listItem = ""
-    for(let i= 0; i<myLead.length; i++){
-        listItem += "<li> <a href = '#' target='_blank'>"+ myLead[i] + "</a></li>"
-     ulEl.innerHTML = listItem
-    }
+
+let myLead = []
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLead = leadsFromLocalStorage
+    renderLeads()
 }
 
-function clearInput(){
-    const clearfield = document.getElementById("input-el")
-    clearfield.value = " "
+inputBtn.addEventListener("click", function () {
+    if (inputEl.value === "") return
+    myLead.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLead))
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItem = ""
+    for (let i = 0; i < myLead.length; i++) {
+        listItem += `<li><a href="${myLead[i]}" target="_blank">${myLead[i]}</a></li>`
+    }
+    ulEl.innerHTML = listItem
 }
+
+deleteBtn.addEventListener("click", function () {
+    localStorage.removeItem("myLeads")
+    myLead = []
+    renderLeads()
+})
